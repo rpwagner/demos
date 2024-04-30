7f1a92f202f2ba9a92c36555884fe58e3ef6e13b
rpwagner <rick@ucsd.edu>
2024-04-30 15:10:21 -0500

    handle code

--- auth.js
+++ auth.js
@@ -12,6 +12,7 @@ console.log("Loaded body");
 
 function globus_login(){
     auth_manager.login();
+    await auth_manager.handleCodeRedirect();
     console.log("Login done");
     document.getElementById("user").innerHTML = "Logged in";
 }
