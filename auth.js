9f1491b4917aed990491b38f903d18b385763e04
rpwagner <rick@ucsd.edu>
2024-04-30 14:38:29 -0500

    redirect to gh pages

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 const auth_config = {
     client: 'f42cc8a6-a1f1-4e75-90e1-612febbb9e7f',
-    redirect: 'http://localhost:8000',
+    redirect: 'https://rpwagner.github.io/demos/',
     scopes: 'openid profile email https://auth.globus.org/scopes/385d3079-5121-40bc-a52f-055296497631/https'};
 
 const auth_manager = new globus.authorization.create(auth_config);
